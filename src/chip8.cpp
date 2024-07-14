848d13033157e9ca7a31194ed26d5a3fb3135776
asherdrake <asher.alacar@gmail.com>
2024-07-14 01:53:21 -0400

    created header file and LoadRom function

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -1,15 +1,35 @@
-#include <cstdint>
+#include "chip8.h"
+#include <fstream>
 
-class Chip8 {
-    public: //'{}' after each variable means initialized to 0
-        uint8_t     registers[16]{};
-        uint8_t     memory[4096]{}; 
-        uint16_t    index;
-        uint16_t    pc;
-        uint16_t    stack[16]{};
-        uint8_t     sp;
-        uint8_t     delayTimer;
-        uint8_t     soundTimer;
-        uint8_t     keypad[16]{};
-        uint32_t    display[64 * 32]{};
-};
+const unsigned int START_ADDRESS = 0x200;
+
+Chip8::Chip8() {
+
+}
+
+void Chip8::LoadRom(char const* filename) {
+    //creates 'file' object
+    //two flags mean: open file as stream of binary, and move file pointer to the end
+    std::ifstream file(filename, std::ios::binary | std::ios::ate);
+    
+    if (file.is_open())
+	{
+		// Get size of file and allocate a buffer to hold the contents
+		std::streampos size = file.tellg();
+		char* buffer = new char[size];
+
+		// Go back to the beginning of the file and fill the buffer
+		file.seekg(0, std::ios::beg);
+		file.read(buffer, size);
+		file.close();
+
+		// Load the ROM contents into the Chip8's memory, starting at 0x200
+		for (long i = 0; i < size; ++i)
+		{
+			memory[START_ADDRESS + i] = buffer[i];
+		}
+
+		// Free the buffer
+		delete[] buffer;
+	}
+}
