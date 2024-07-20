c42638795cd27022ae8eba3a6e1c6c3a644e7eab
asherdrake <asher.alacar@gmail.com>
2024-07-19 23:05:51 -0400

    added function pointer table

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -4,8 +4,87 @@
 const unsigned int START_ADDRESS = 0x200;
 
 Chip8::Chip8() {
+    //function pointer table setup
+    //unique opcodes
+    table[0x0] = &Chip8::Table0;
+    table[0x1] = &Chip8::OP_1nnn;
+    table[0x2] = &Chip8::OP_2nnn;
+    table[0x3] = &Chip8::OP_3xkk;
+    table[0x4] = &Chip8::OP_4xkk;
+    table[0x5] = &Chip8::OP_5xy0;
+    table[0x6] = &Chip8::OP_6xkk;
+    table[0x7] = &Chip8::OP_7xkk;
+    table[0x8] = &Chip8::Table8;
+    table[0x9] = &Chip8::OP_9xy0;
+    table[0xA] = &Chip8::OP_Annn;
+    table[0xB] = &Chip8::OP_Bnnn;
+    table[0xC] = &Chip8::OP_Cxkk;
+    table[0xD] = &Chip8::OP_Dxyn;
+    table[0xE] = &Chip8::TableE;
+    table[0xF] = &Chip8::TableF;
 
+    //initializing entries up to 0xE
+    for (size_t i = 0; i <= 0xE; i++) {
+        table0[i] = &Chip8::OP_NULL;
+        table8[i] = &Chip8::OP_NULL;
+        tableE[i] = &Chip8::OP_NULL;
+    }
+
+    //table0 opcodes
+    table0[0x0] = &Chip8::OP_00E0;
+    table0[0xE] = &Chip8::OP_00EE;
+
+    //table8 opcodes
+    table8[0x0] = &Chip8::OP_8xy0;
+    table8[0x1] = &Chip8::OP_8xy1;
+    table8[0x2] = &Chip8::OP_8xy2;
+    table8[0x3] = &Chip8::OP_8xy3;
+    table8[0x4] = &Chip8::OP_8xy4;
+    table8[0x5] = &Chip8::OP_8xy5;
+    table8[0x6] = &Chip8::OP_8xy6;
+    table8[0x7] = &Chip8::OP_8xy7;
+    table8[0xE] = &Chip8::OP_8xyE;
+
+    //tableE opcodes
+    tableE[0x1] = &Chip8::OP_ExA1;
+    tableE[0xE] = &Chip8::OP_Ex9E;
+
+    //initializing tableF up to 0x65
+    for (size_t i = 0; i <= 0x65; i++) {
+        tableF[i] = &Chip8::OP_NULL;
+    }
+
+    //tableF opcodes
+    tableF[0x07] = &Chip8::OP_Fx07;
+    tableF[0x0A] = &Chip8::OP_Fx0A;
+    tableF[0x15] = &Chip8::OP_Fx15;
+    tableF[0x18] = &Chip8::OP_Fx18;
+    tableF[0x1E] = &Chip8::OP_Fx1E;
+    tableF[0x29] = &Chip8::OP_Fx29;
+    tableF[0x33] = &Chip8::OP_Fx33;
+    tableF[0x55] = &Chip8::OP_Fx55;
+    tableF[0x65] = &Chip8::OP_Fx65;
+
+    pc = START_ADDRESS; //start at 0x200, since 0x000 to 0x1FF is reserved
 }
+
+void Chip8::Table0() {
+	((*this).*(table0[opcode & 0x000Fu]))();
+}
+
+void Chip8::Table8() {
+    ((*this).*(table8[opcode & 0x000Fu]))();
+}
+
+void Chip8::TableE() {
+    ((*this).*(tableE[opcode & 0x000Fu]))();
+}
+
+void Chip8::TableF() {
+    ((*this).*(tableF[opcode & 0x00FFu]))();
+}
+
+void Chip8::OP_NULL() {}
 
 void Chip8::LoadRom(char const* filename) {
     //creates 'file' object
@@ -15,12 +94,14 @@ void Chip8::LoadRom(char const* filename) {
     if (file.is_open())
 	{
 		// Get size of file and allocate a buffer to hold the contents
+        //file.tellg() returns the current position of the file pointer (which is the end, due to std::ios::ate)
+        //since the pointer is at the end, it gives the size of the file in bytes
 		std::streampos size = file.tellg();
 		char* buffer = new char[size];
 
 		// Go back to the beginning of the file and fill the buffer
-		file.seekg(0, std::ios::beg);
-		file.read(buffer, size);
+		file.seekg(0, std::ios::beg); //moves file pointer to offset of 0 from beginning of file
+		file.read(buffer, size);      
 		file.close();
 
 		// Load the ROM contents into the Chip8's memory, starting at 0x200
@@ -33,3 +114,6 @@ void Chip8::LoadRom(char const* filename) {
 		delete[] buffer;
 	}
 }
+
+//instruction functions
+void Chip8::OP_00E0(){}
--- src/chip8.h
+++ src/chip8.h
@@ -1,7 +1,13 @@
+#ifndef CHIP8_H
+#define CHIP8_H
+
 #include <cstdint>
 
 class Chip8 {
     public:
+        Chip8();
+
+    private:
         uint8_t registers[16]{};
         uint8_t memory[4096]{};
         uint16_t index{};
@@ -14,5 +20,67 @@ class Chip8 {
         uint32_t video[64 * 32]{};
         uint16_t opcode;
 
+        //function pointer tables (NEEDS IMPLEMENTATION)
+        void Table0(); 
+        void Table8(); 
+        void TableE();  
+        void TableF();   
+        void OP_NULL(); 
+ 
+        typedef void (Chip8::*Chip8Func)();
+        Chip8Func table[0xF + 1];
+        Chip8Func table0[0xE + 1];
+        Chip8Func table8[0xE + 1];
+        Chip8Func tableE[0xE + 1];
+        Chip8Func tableF[0x65 + 1];
+
+        //instruction functions (NEEDS IMPLEMENTATION)
+
+        //table0 opcodes
+        void OP_00E0();
+        void OP_00EE();
+
+        //table8 opcodes
+        void OP_8xy0();
+        void OP_8xy1();
+        void OP_8xy2();
+        void OP_8xy3();
+        void OP_8xy4();
+        void OP_8xy5();
+        void OP_8xy6();
+        void OP_8xy7();
+        void OP_8xyE();
+
+        //tableE opcodes
+        void OP_Ex9E();
+        void OP_ExA1();
+
+        //tableF functions
+        void OP_Fx07();
+        void OP_Fx0A();
+        void OP_Fx15();
+        void OP_Fx18();
+        void OP_Fx1E();
+        void OP_Fx29();
+        void OP_Fx33();
+        void OP_Fx55();
+        void OP_Fx65();
+
+        //unique opcodes
+        void OP_1nnn();
+        void OP_2nnn();
+        void OP_3xkk();
+        void OP_4xkk();
+        void OP_5xy0();
+        void OP_6xkk();
+        void OP_7xkk();
+        void OP_9xy0();
+        void OP_Annn();
+        void OP_Bnnn();
+        void OP_Cxkk();
+        void OP_Dxyn();
+        
     void LoadRom(char const* filename) {}
 };
+
+#endif
