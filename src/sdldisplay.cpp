01fe09a5071970b66ca434eb86e2f7663470057d
asherdrake <asher.alacar@gmail.com>
2024-07-25 15:35:58 -0700

    setup SDLDisplay class

---
+++ src/sdldisplay.cpp
@@ -0,0 +1,5 @@
+#include "sdldisplay.h"
+
+SDLDisplay::SDLDisplay() {
+
+};
---
+++ src/sdldisplay.h
@@ -0,0 +1,10 @@
+#ifndef SDLDISPLAY_H
+#define SDLDISPLAY_H
+
+#include <SDL2/SDL.h>
+
+class SDLDisplay {
+
+};
+
+#endif
